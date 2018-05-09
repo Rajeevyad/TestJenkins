ddfee9c9d27b4b5db83644ef22ce79ab1d2380f1
ryadav46 <ryadav46@csc.com>
2018-05-09 17:46:44 +0530

    Added Jenkins file

---
+++ dockercompose.ps1
@@ -0,0 +1,15 @@
+Write-Host "Stopping all container ......................"
+docker stop $(docker ps -aq)
+
+Write-Host "Removing all container ......................"
+docker rm $(docker ps -aq)
+
+Write-Host "Removing previous built images......................"
+docker-compose -f docker-compose.yml -f docker-compose.override.yml down
+
+Write-Host "Check config of container......................"
+docker-compose -f docker-compose.yml -f docker-compose.override.yml config
+
+Write-Host "Build and run docker container........."
+docker-compose -f docker-compose.yml -f docker-compose.override.yml up --build -d
+
